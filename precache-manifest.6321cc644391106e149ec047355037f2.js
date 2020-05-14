ddefaf9556ed5e39841c6480b96ecbc83a2532c4
Oteri Eyenike <teyenike1@gmail.com>
2020-05-14 04:41:08 +0100

    Updates

---
+++ precache-manifest.6321cc644391106e149ec047355037f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63b8a2b75fc36472d781f758cf40a15",
+    "url": "/progate-react/index.html"
+  },
+  {
+    "revision": "69e5db88c33160e56b92",
+    "url": "/progate-react/static/css/main.3ff7de74.chunk.css"
+  },
+  {
+    "revision": "37b8f72d2c17d6194c56",
+    "url": "/progate-react/static/js/2.db566fda.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/progate-react/static/js/2.db566fda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69e5db88c33160e56b92",
+    "url": "/progate-react/static/js/main.31c7c3c7.chunk.js"
+  },
+  {
+    "revision": "e8eeb782d185e037977d",
+    "url": "/progate-react/static/js/runtime-main.c29924e1.js"
+  },
+  {
+    "revision": "a02452e14625b5af8dbcb37a3cc3ffbb",
+    "url": "/progate-react/static/media/progate.a02452e1.svg"
+  },
+  {
+    "revision": "7f995bc8faf05bc72aad13c551d939ef",
+    "url": "/progate-react/static/media/progate_lg.7f995bc8.svg"
+  }
+]);
